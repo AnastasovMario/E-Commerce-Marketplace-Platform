516186b3bcffe1f77352f2f7125e1c3d4ac23ba1
unknown <124439378+AnastasovMario@users.noreply.github.com>
2023-08-09 23:44:11 +0300

    Add admin Area

---
+++ E-Commerce Marketplace Platform/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Marketplace_Platform.Areas.Admin.Controllers
+{
+    
+    public class AdminController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Commerce Marketplace Platform/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Marketplace_Platform.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Administrator")]
+	public class BaseController : Controller
+	{
+	}
+}
